480d8744529219facb98577f8f3f82b9fa23d965
Jacob Peng <jacob.peng@hotmail.com>
2021-07-07 11:15:38 -0400

    Add benchmarks against Enzyme

--- scripts/run_benchmark.sh
+++ scripts/run_benchmark.sh
@@ -13,15 +13,22 @@ BUFFERIZE="-tensor-constant-bufferize -linalg-bufferize -canonicalize -func-buff
 LOWERING="-convert-linalg-to-loops -finalizing-bufferize -convert-linalg-to-llvm"
 OPT_ARGS="$TENSOR_PREPROCESSING $BUFFERIZE $LOWERING"
 
+# Compile the Enzyme object file
+ENZYME_DYLIB=/Users/Appleliu/llvm-project/mlir/examples/playground/enzyme_test/LLVMEnzyme-12.dylib
+
+clang -S -emit-llvm "$DRIVERS/enzyme_$FILE.c" -o "$TMP_DIR/preenzyme.ll"
+opt "$TMP_DIR/preenzyme.ll" -load "$ENZYME_DYLIB" -enzyme -o "$TMP_DIR/postenzyme.ll" -S
+clang -c "$TMP_DIR/postenzyme.ll" -o "$TMP_DIR/postenzyme.o"
+
 # For debugging
 # $BIN/standalone-opt "$MLIR_KERNELS/$FILE.mlir" $TENSOR_PREPROCESSING $BUFFERIZE
 
+# For execution
 $BIN/standalone-opt "$MLIR_KERNELS/$FILE.mlir" -take-grads $OPT_ARGS --llvm-legalize-for-export | mlir-translate -mlir-to-llvmir > "$LLVM_FILE"
 llc -filetype=obj < "$LLVM_FILE" > "$OBJ_FILE"
 
 CC=gcc
 "$CC" -c "$DRIVERS/$FILE.c" -o "$TMP_DIR/driver.o"
-"$CC" "$TMP_DIR/driver.o" "$OBJ_FILE" -o "$TMP_DIR/$FILE.out"
+"$CC" "$TMP_DIR/driver.o" "$OBJ_FILE" "$TMP_DIR/postenzyme.o" -o "$TMP_DIR/$FILE.out"
 
 "$TMP_DIR/$FILE.out"
- 
--- test/Driver/dot.c
+++ test/Driver/dot.c
@@ -3,11 +3,11 @@
  */
 
 #include <math.h>
+#include <stdbool.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/time.h>
-
-typedef long long int64_t;
 
 typedef struct {
   float *allocated;
@@ -25,6 +25,7 @@ typedef struct {
 extern float mlir_sum(float *, float *, int64_t, int64_t, int64_t);
 extern F32MemRef ddot(float *, float *, int64_t, int64_t, int64_t, float *,
                       float *, int64_t, int64_t, int64_t);
+extern void denzyme_dot(int, float *, float *, float *, float *);
 
 void random_init(float *arr, int size) {
   for (int i = 0; i < size; ++i) {
@@ -32,25 +33,67 @@ void random_init(float *arr, int size) {
   }
 }
 
+static inline unsigned long timediff(struct timeval start, struct timeval stop) {
+  return (stop.tv_sec - start.tv_sec) * 1000000 + stop.tv_usec - start.tv_usec;
+}
+
+void print_arr(unsigned long *arr, int n) {
+  printf("[");
+  for (int i = 0; i < n; i++) {
+    printf("%lu", arr[i]);
+    if (i != n - 1) {
+      printf(", ");
+    }
+  }
+  printf("]\n");
+}
+
 int main() {
   const int size = 32768;
-  float a[size];
-  float b[size];
-  random_init(a, size);
-  random_init(b, size);
+  const int NUM_RUNS = 20;
+  bool check_val = false;
 
-  float *deadbeef = (float *)0xdeadbeef;
-  struct timeval stop, start;
+  unsigned long grad_results[NUM_RUNS];
+  unsigned long enzyme_results[NUM_RUNS];
 
-  gettimeofday(&start, NULL);
-  F32MemRef dot_grad = ddot(deadbeef, a, 0, size, 1, deadbeef, b, 0, size, 1);
-  gettimeofday(&stop, NULL);
+  for (int run = 0; run < NUM_RUNS; run++) {
+    float a[size];
+    float b[size];
+    random_init(a, size);
+    random_init(b, size);
 
-  printf("Grad took %lu us\n",
-         (stop.tv_sec - start.tv_sec) * 1000000 + stop.tv_usec - start.tv_usec);
-  float error = 0;
-  for (int i = 0; i < dot_grad.descriptor->sizes; ++i) {
-    error += fabs(dot_grad.descriptor->aligned[i] - b[i]);
+    float *deadbeef = (float *)0xdeadbeef;
+    struct timeval stop, start;
+
+    gettimeofday(&start, NULL);
+    F32MemRef dot_grad = ddot(deadbeef, a, 0, size, 1, deadbeef, b, 0, size, 1);
+    gettimeofday(&stop, NULL);
+
+    grad_results[run] = timediff(start, stop);
+    if (check_val) {
+      float error = 0;
+      for (int i = 0; i < dot_grad.descriptor->sizes; ++i) {
+        error += fabs(dot_grad.descriptor->aligned[i] - b[i]);
+      }
+      printf("Grad total absolute error: %f\n", error);
+    }
+
+    gettimeofday(&start, NULL);
+    float da[size] = {0};
+    float db[size] = {0};
+    denzyme_dot(size, a, da, b, db);
+    gettimeofday(&stop, NULL);
+
+    enzyme_results[run] = timediff(start, stop);
+    if (check_val) {
+      float error = 0;
+      for (int i = 0; i < size; i++) {
+        error += fabs(da[i] - b[i]);
+      }
+      printf("Enzyme total absolute error: %f\n", error);
+    }
   }
-  printf("Total absolute error: %f\n", error);
+
+  print_arr(grad_results, NUM_RUNS);
+  print_arr(enzyme_results, NUM_RUNS);
 }
