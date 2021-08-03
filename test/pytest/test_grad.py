772f6b19844567f575879c434d388612488b723c
Jacob Peng <jacob.peng@hotmail.com>
2021-08-03 16:16:43 -0400

    Add test for matvec gradient

--- test/pytest/test_grad.py
+++ test/pytest/test_grad.py
@@ -1,6 +1,7 @@
+import numpy as np
 import os.path as osp
 from toolchain import compile_pipeline
-from stdout_parser import extract_scalar, extract_1d
+from stdout_parser import extract_scalar, extract_1d, extract_2d
 
 MLIR_FILES = osp.join(osp.dirname(__file__), "..", "Standalone")
 
@@ -23,3 +24,11 @@ def test_dot():
 def test_dot_second_arg():
     output = compile_pipeline(f"{MLIR_FILES}/dot_second_arg.mlir", mode="grad")
     assert extract_1d(output.decode("utf-8")) == [0.1, 1.0, 2.0, -3.0]
+
+
+def test_matvec_with_postmul():
+    output = compile_pipeline(f"{MLIR_FILES}/matvec.mlir", mode="grad")
+    assert np.allclose(
+        extract_2d(output.decode("utf-8")),
+        np.outer([1.1, -1.2, 1.0], [-1.2, -1.3, 1.5, 2.2]),
+    )
