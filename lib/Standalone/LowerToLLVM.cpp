de11a4b95142e18e0745aa97a2f0dc95fc33f108
Jacob Peng <jacob.peng@hotmail.com>
2021-09-27 18:07:05 -0400

    Add explicit conversion casts when lowering to Enzyme

--- lib/Standalone/LowerToLLVM.cpp
+++ lib/Standalone/LowerToLLVM.cpp
@@ -5,11 +5,11 @@
 #include "mlir/Conversion/AffineToStandard/AffineToStandard.h"
 #include "mlir/Conversion/LLVMCommon/ConversionTarget.h"
 #include "mlir/Conversion/LLVMCommon/TypeConverter.h"
+#include "mlir/Conversion/LinalgToLLVM/LinalgToLLVM.h"
+#include "mlir/Conversion/MathToLLVM/MathToLLVM.h"
+#include "mlir/Conversion/MemRefToLLVM/MemRefToLLVM.h"
 #include "mlir/Conversion/SCFToStandard/SCFToStandard.h"
 #include "mlir/Conversion/StandardToLLVM/ConvertStandardToLLVM.h"
-#include "mlir/Conversion/MathToLLVM/MathToLLVM.h"
-#include "mlir/Conversion/LinalgToLLVM/LinalgToLLVM.h"
-#include "mlir/Conversion/MemRefToLLVM/MemRefToLLVM.h"
 #include "mlir/Dialect/Affine/IR/AffineOpsDialect.h.inc"
 #include "mlir/Dialect/LLVMIR/LLVMDialect.h"
 #include "mlir/Dialect/StandardOps/IR/Ops.h"
@@ -42,6 +42,10 @@ public:
     // operator and replace it with a call to the Enzyme API
     auto result = op->getResult(0);
     rewriter.eraseOp(op);
+
+    // TODO: How are we supposed to populate the type converter member?
+    LLVMTypeConverter typeConverter(op->getContext());
+
     for (auto it = result.use_begin(); it != result.use_end(); it++) {
       // TODO: We currently assume that user is a call_indirect op.
       auto user = it.getUser();
@@ -60,43 +64,57 @@ public:
           auto rank = memrefType.getRank();
           // Ignore the first pointer
           arguments.push_back(enzyme_const_addr.getResult());
-          arguments.push_back(
-              rewriter
-                  .create<LLVM::ExtractValueOp>(user->getLoc(), llvmF32Ptr, arg,
-                                                rewriter.getI64ArrayAttr(0))
-                  .getResult());
-          arguments.push_back(
-              rewriter
-                  .create<LLVM::ExtractValueOp>(user->getLoc(), llvmF32Ptr, arg,
-                                                rewriter.getI64ArrayAttr(1))
-                  .getResult());
+          auto casted = rewriter
+                            .create<UnrealizedConversionCastOp>(
+                                user->getLoc(),
+                                typeConverter.convertType(arg.getType()), arg)
+                            .getResult(0);
+          arguments.push_back(rewriter
+                                  .create<LLVM::ExtractValueOp>(
+                                      user->getLoc(), llvmF32Ptr, casted,
+                                      rewriter.getI64ArrayAttr(0))
+                                  .getResult());
+          arguments.push_back(rewriter
+                                  .create<LLVM::ExtractValueOp>(
+                                      user->getLoc(), llvmF32Ptr, casted,
+                                      rewriter.getI64ArrayAttr(1))
+                                  .getResult());
 
           // Shadow pointer has to follow the aligned pointer
           auto shadow = *(++it);
           assert(shadow && shadow.getType().isa<MemRefType>() &&
                  "Shadow argument must be a Memref");
+          auto shadowCasted =
+              rewriter
+                  .create<UnrealizedConversionCastOp>(
+                      shadow.getLoc(),
+                      typeConverter.convertType(shadow.getType()), shadow)
+                  .getResult(0);
           auto extractShadowOp = rewriter.create<LLVM::ExtractValueOp>(
-              shadow.getLoc(), llvmF32Ptr, shadow, rewriter.getI64ArrayAttr(1));
+              shadow.getLoc(), llvmF32Ptr, shadowCasted,
+              rewriter.getI64ArrayAttr(1));
           arguments.push_back(extractShadowOp.getResult());
 
           auto llvmI64Ty = IntegerType::get(user->getContext(), 64);
-          arguments.push_back(
-              rewriter
-                  .create<LLVM::ExtractValueOp>(user->getLoc(), llvmI64Ty, arg,
-                                                rewriter.getI64ArrayAttr(2))
-                  .getResult());
-          for (int64_t i = 0; i < rank; ++i)
+          arguments.push_back(rewriter
+                                  .create<LLVM::ExtractValueOp>(
+                                      user->getLoc(), llvmI64Ty, casted,
+                                      rewriter.getI64ArrayAttr(2))
+                                  .getResult());
+          for (int64_t i = 0; i < rank; ++i) {
             arguments.push_back(rewriter
                                     .create<LLVM::ExtractValueOp>(
-                                        user->getLoc(), llvmI64Ty, arg,
+                                        user->getLoc(), llvmI64Ty, casted,
                                         rewriter.getI64ArrayAttr({3, i}))
                                     .getResult());
-          for (int64_t i = 0; i < rank; ++i)
+          }
+          for (int64_t i = 0; i < rank; ++i) {
             arguments.push_back(rewriter
                                     .create<LLVM::ExtractValueOp>(
-                                        user->getLoc(), llvmI64Ty, arg,
+                                        user->getLoc(), llvmI64Ty, casted,
                                         rewriter.getI64ArrayAttr({4, i}))
                                     .getResult());
+          }
         } else {
           arguments.push_back(arg);
         }
@@ -183,17 +201,17 @@ void StandaloneToLLVMLoweringPass::runOnOperation() {
   LLVMTypeConverter typeConverter(&getContext());
 
   OwningRewritePatternList patterns(&getContext());
-  populateAffineToStdConversionPatterns(patterns);
+  // populateAffineToStdConversionPatterns(patterns);
   populateLoopToStdConversionPatterns(patterns);
+  populateMathToLLVMConversionPatterns(typeConverter, patterns);
   populateMemRefToLLVMConversionPatterns(typeConverter, patterns);
   // populateLinalgToLLVMConversionPatterns(typeConverter, patterns);
   populateStdToLLVMConversionPatterns(typeConverter, patterns);
-  // populateStdToLLVMFuncOpConversionPattern(typeConverter, patterns);
-  populateMathToLLVMConversionPatterns(typeConverter, patterns);
   patterns.insert<DiffOpLowering>(&getContext());
+  // populateStdToLLVMFuncOpConversionPattern(typeConverter, patterns);
 
   auto mod = getOperation();
-  if (failed(applyFullConversion(mod, target, std::move(patterns))))
+  if (failed(applyPartialConversion(mod, target, std::move(patterns))))
     signalPassFailure();
 }
 
