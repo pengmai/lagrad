b60b1c7c4c10553889dccdbcb1a344e7f8fe73c5
Jacob Peng <jacob.peng@hotmail.com>
2021-10-01 14:11:32 -0400

    Fix various bugs with GMM correctness verification

--- benchmarks/C/templates/gmm.h
+++ benchmarks/C/templates/gmm.h
@@ -1,10 +1,89 @@
 #pragma once
 #include "mlir_c_abi.h"
+#include "shared_types.h"
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 
+#define FILENAME "benchmarks/data/gmm_d10_K25.txt"
 #define GRAD_FILENAME "benchmarks/data/gmm_results.txt"
+
+GMMInput read_gmm_data() {
+  FILE *fp;
+  GMMInput gmm_input;
+  fp = fopen(FILENAME, "r");
+  if (fp == NULL) {
+    fprintf(stderr, "Failed to open file \"%s\"\n", FILENAME);
+    exit(EXIT_FAILURE);
+  }
+
+  int d, k, n;
+  fscanf(fp, "%d %d %d", &d, &k, &n);
+
+  int icf_sz = d * (d + 1) / 2;
+  double *alphas = (double *)malloc(k * sizeof(double));
+  double *means = (double *)malloc(d * k * sizeof(double));
+  double *icf = (double *)malloc(icf_sz * k * sizeof(double));
+  double *Qs = (double *)malloc(k * d * sizeof(double));
+  double *Ls = (double *)malloc(k * d * d * sizeof(double));
+  for (int i = 0; i < k * d * d; i++) {
+    Ls[i] = 0;
+  }
+
+  double *x = (double *)malloc(d * n * sizeof(double));
+
+  for (int i = 0; i < k; i++) {
+    fscanf(fp, "%lf", &alphas[i]);
+  }
+
+  for (int i = 0; i < k; i++) {
+    for (int j = 0; j < d; j++) {
+      fscanf(fp, "%lf", &means[i * d + j]);
+    }
+  }
+
+  for (int i = 0; i < k; i++) {
+    int Lcol = 0;
+    int Lidx = 0;
+    for (int j = 0; j < icf_sz; j++) {
+      fscanf(fp, "%lf", &icf[i * icf_sz + j]);
+      if (j < d) {
+        Qs[i * d + j] = icf[i * icf_sz + j];
+      } else {
+        Ls[i * d * d + (Lidx + 1) * d + Lcol] = icf[i * icf_sz + j];
+        Lidx++;
+        if (Lidx == d - 1) {
+          Lcol++;
+          Lidx = Lcol;
+        }
+      }
+    }
+  }
+
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < d; j++) {
+      fscanf(fp, "%lf", &x[i * d + j]);
+    }
+  }
+
+  int wishart_m;
+  double wishart_gamma;
+  fscanf(fp, "%lf %d", &wishart_gamma, &wishart_m);
+  fclose(fp);
+
+  gmm_input.d = d;
+  gmm_input.k = k;
+  gmm_input.n = n;
+  gmm_input.alphas = alphas;
+  gmm_input.means = means;
+  gmm_input.Qs = Qs;
+  gmm_input.Ls = Ls;
+  gmm_input.icf = icf;
+  gmm_input.x = x;
+  gmm_input.wishart_gamma = wishart_gamma;
+  gmm_input.wishart_m = wishart_m;
+  return gmm_input;
+}
 
 void read_gmm_grads(size_t d, size_t k, size_t n, double *dalphas,
                     double *dmeans, double *dicf) {
@@ -88,16 +167,15 @@ void check_gmm_err(size_t d, size_t k, size_t n, double *dalphas,
     printf("(%s) means err: %f\n", app, means_err);
   }
 
-  double icf_err = 0.0;
   int icf_sz = d * (d + 1) / 2;
+  double max_icf_err = -1;
   for (size_t i = 0; i < k * icf_sz; i++) {
-    if (i < 10) {
-      printf("expected: %f actual: %f\n", ref_icf[i], dicf[i]);
+    if (fabs(ref_icf[i] - dicf[i]) > max_icf_err) {
+      max_icf_err = ref_icf[i] - dicf[i];
     }
-    icf_err += fabs(ref_icf[i] - dicf[i]);
   }
-  if (icf_err > 1e-4) {
-    printf("(%s) icf err: %f\n", app, icf_err);
+  if (max_icf_err > 1e-5) {
+    printf("(%s) max icf err: %f\n", app, max_icf_err);
   }
 }
 
@@ -111,20 +189,12 @@ void check_gmm_err(size_t d, size_t k, size_t n, double *dalphas,
  * becomes:
  * [a b c d e f]
  */
-void collapse_ltri(size_t k, size_t d, double *Ls, double *out, size_t outer) {
-  // icf_size = 55, d = 10, k = 25
-  int ltri_size = d * (d - 1) / 2;
-  for (size_t i = 0; i < k; i++) {
-    size_t icf_idx = 0;
-    for (size_t j = 0; j < d; j++) {
-      for (size_t l = j + 1; l < d; l++) {
-        // out[i * ltri_size + icf_idx] = Ls[(i * d * d) + (l * d) + j];
-        out[i * ltri_size + icf_idx] = Ls[(l * d) + j];
-        if (outer == k - 1 && i == 0) {
-          printf("(collapse ltri) out index: %lu\n", i * ltri_size + icf_idx);
-        }
-        icf_idx++;
-      }
+void collapse_ltri(size_t k, size_t d, double *Ls, double *out) {
+  size_t icf_idx = 0;
+  for (size_t j = 0; j < d; j++) {
+    for (size_t l = j + 1; l < d; l++) {
+      out[icf_idx] = Ls[(l * d) + j];
+      icf_idx++;
     }
   }
 }
@@ -139,7 +209,6 @@ void convert_ql_to_icf(size_t d, size_t k, size_t n, double *Qs, double *Ls,
     for (size_t d1 = 0; d1 < d; d1++) {
       icf[d0 * icf_sz + d1] = Qs[d0 * d + d1];
     }
-    collapse_ltri(k, d, &Ls[d0 * d * d], &icf[d0 * icf_sz + d], d0);
+    collapse_ltri(k, d, &Ls[d0 * d * d], &icf[d0 * icf_sz + d]);
   }
-  print_d_arr(icf + icf_sz, icf_sz);
 }
--- benchmarks/C/templates/gmm_driver.c
+++ benchmarks/C/templates/gmm_driver.c
@@ -8,9 +8,8 @@
 #include <string.h>
 #include <sys/time.h>
 
-#define FILENAME "benchmarks/data/gmm_d10_K25.txt"
-#define NUM_RUNS 1
-#define NUM_WARMUPS 0
+#define NUM_RUNS 20
+#define NUM_WARMUPS 20
 
 typedef struct {
   double *data;
@@ -50,86 +49,6 @@ extern void enzyme_gmm_objective_full_L(int d, int k, int n, double *alphas,
                                         double *x, double wishart_gamma,
                                         int wishart_m, double *err);
 
-GMMInput read_data() {
-  FILE *fp;
-  GMMInput gmm_input;
-  fp = fopen(FILENAME, "r");
-  if (fp == NULL) {
-    fprintf(stderr, "Failed to open file \"%s\"\n", FILENAME);
-    exit(EXIT_FAILURE);
-  }
-
-  int d, k, n;
-  fscanf(fp, "%d %d %d", &d, &k, &n);
-
-  // For some reason, this is consistent with Enzyme's implementation but the
-  // original paper has d * (d - 1) / 2. When I change this value to "fix" it,
-  // the gradient computation goes off slightly.
-  int icf_sz = d * (d + 1) / 2;
-  double *alphas = (double *)malloc(k * sizeof(double));
-  double *means = (double *)malloc(d * k * sizeof(double));
-  double *icf = (double *)malloc(icf_sz * k * sizeof(double));
-  double *Qs = (double *)malloc(k * d * sizeof(double));
-  double *Ls = (double *)malloc(k * d * d * sizeof(double));
-  for (int i = 0; i < k * d * d; i++) {
-    Ls[i] = 0;
-  }
-
-  double *x = (double *)malloc(d * n * sizeof(double));
-
-  for (int i = 0; i < k; i++) {
-    fscanf(fp, "%lf", &alphas[i]);
-  }
-
-  for (int i = 0; i < k; i++) {
-    for (int j = 0; j < d; j++) {
-      fscanf(fp, "%lf", &means[i * d + j]);
-    }
-  }
-
-  for (int i = 0; i < k; i++) {
-    int Lcol = 0;
-    int Lidx = 0;
-    for (int j = 0; j < icf_sz; j++) {
-      fscanf(fp, "%lf", &icf[i * icf_sz + j]);
-      if (j < d) {
-        Qs[i * d + j] = icf[i * icf_sz + j];
-      } else {
-        Ls[i * d * d + (Lidx + 1) * d + Lcol] = icf[i * icf_sz + j];
-        Lidx++;
-        if (Lidx == d - 1) {
-          Lcol++;
-          Lidx = Lcol;
-        }
-      }
-    }
-  }
-
-  for (int i = 0; i < n; i++) {
-    for (int j = 0; j < d; j++) {
-      fscanf(fp, "%lf", &x[i * d + j]);
-    }
-  }
-
-  int wishart_m;
-  double wishart_gamma;
-  fscanf(fp, "%lf %d", &wishart_gamma, &wishart_m);
-  fclose(fp);
-
-  gmm_input.d = d;
-  gmm_input.k = k;
-  gmm_input.n = n;
-  gmm_input.alphas = alphas;
-  gmm_input.means = means;
-  gmm_input.Qs = Qs;
-  gmm_input.Ls = Ls;
-  gmm_input.icf = icf;
-  gmm_input.x = x;
-  gmm_input.wishart_gamma = wishart_gamma;
-  gmm_input.wishart_m = wishart_m;
-  return gmm_input;
-}
-
 void free_gmm_input(GMMInput gmm_input) {
   free(gmm_input.alphas);
   free(gmm_input.means);
@@ -139,7 +58,7 @@ void free_gmm_input(GMMInput gmm_input) {
 }
 
 int main() {
-  GMMInput gmm_input = read_data();
+  GMMInput gmm_input = read_gmm_data();
   int d = gmm_input.d;
   int k = gmm_input.k;
   int n = gmm_input.n;
@@ -158,7 +77,7 @@ int main() {
 
   double *ref_alphas = (double *)malloc(k * sizeof(double));
   double *ref_means = (double *)malloc(d * k * sizeof(double));
-  double *ref_icf = (double *)malloc(icf_size * k * sizeof(double));
+  double *ref_icf = (double *)malloc(k * icf_size * sizeof(double));
 
   read_gmm_grads(d, k, n, ref_alphas, ref_means, ref_icf);
 
@@ -176,49 +95,31 @@ int main() {
     gettimeofday(&stop, NULL);
 
     grad_results[run] = timediff(start, stop);
-    // printf("Before conversion of icfb:\n");
-    // print_d_arr(ref_icf, icf_size);
     convert_ql_to_icf(d, k, n, lagrad_result.dqs.aligned,
                       lagrad_result.dls.aligned, icfb);
-    // printf("After conversion:\n");
-    // print_d_arr(ref_icf, icf_size);
     check_gmm_err(d, k, n, lagrad_result.dalphas.aligned, ref_alphas,
                   lagrad_result.dmeans.aligned, ref_means, icfb, ref_icf,
                   "LAGrad");
     // serialize_gmm_grads(d, k, n, lagrad_result.dalphas.aligned,
-    //                     lagrad_result.dmeans.aligned, ref_icf);
+    //                     lagrad_result.dmeans.aligned, icfb);
 
     gettimeofday(&start, NULL);
-    // double err = 0.0, errb = 1.0;
-    // for (size_t i = 0; i < k; i++) {
-    //   alphasb[i] = 0;
-    // }
-    // for (size_t i = 0; i < d * k; i++) {
-    //   meansb[i] = 0;
-    // }
-    // for (size_t i = 0; i < icf_size * k; i++) {
-    //   icfb[i] = 0;
-    // }
-    // dgmm_objective(&gmm_input, alphasb, meansb, icfb, &err, &errb);
+    double err = 0.0, errb = 1.0;
+    for (size_t i = 0; i < k; i++) {
+      alphasb[i] = 0;
+    }
+    for (size_t i = 0; i < d * k; i++) {
+      meansb[i] = 0;
+    }
+    for (size_t i = 0; i < icf_size * k; i++) {
+      icfb[i] = 0;
+    }
+    dgmm_objective(&gmm_input, alphasb, meansb, icfb, &err, &errb);
     gettimeofday(&stop, NULL);
 
     enzyme_results[run] = timediff(start, stop);
-
-    double alphas_err = 0.0;
-    for (size_t i = 0; i < k; i++) {
-      // printf("");
-      alphas_err += fabs(alphasb[i] - lagrad_result.dalphas.aligned[i]);
-    }
-    if (alphas_err > 1e-6) {
-      // printf("(Run %lu) Alphas err: %f\n", run, alphas_err);
-    }
-    double means_err = 0.0;
-    for (size_t i = 0; i < d * k; i++) {
-      means_err += fabs(meansb[i] - lagrad_result.dmeans.aligned[i]);
-    }
-    if (means_err > 1e-6) {
-      // printf("(Run %lu) Means err: %f\n", run, means_err);
-    }
+    check_gmm_err(d, k, n, alphasb, ref_alphas, meansb, ref_means, icfb,
+                  ref_icf, "Enzyme");
 
     gettimeofday(&start, NULL);
     // err = 0.0, errb = 1.0;
@@ -244,41 +145,24 @@ int main() {
     free(lagrad_result.dls.aligned);
   }
 
-  // double lagrad_avg = 0.0;
-  // double enzyme_avg = 0.0;
+  double lagrad_avg = 0.0;
+  double enzyme_avg = 0.0;
   // double enzyme_notri_avg = 0.0;
-  // for (size_t run = NUM_WARMUPS; run < NUM_WARMUPS + NUM_RUNS; run++) {
-  //   lagrad_avg += grad_results[run];
-  //   enzyme_notri_avg += enzyme_notri_results[run];
-  //   enzyme_avg += enzyme_results[run];
-  // }
-  // printf("LAGrad avg: %f\n", lagrad_avg / NUM_RUNS);
-  // printf("Enzyme avg: %f\n", enzyme_avg / NUM_RUNS);
+  for (size_t run = NUM_WARMUPS; run < NUM_WARMUPS + NUM_RUNS; run++) {
+    lagrad_avg += grad_results[run];
+    // enzyme_notri_avg += enzyme_notri_results[run];
+    enzyme_avg += enzyme_results[run];
+  }
+  printf("LAGrad avg: %f\n", lagrad_avg / NUM_RUNS);
+  printf("Enzyme avg: %f\n", enzyme_avg / NUM_RUNS);
   // printf("Enzyme avg (notri): %f\n", enzyme_notri_avg / NUM_RUNS);
-  // printf("Slowdown: %f\n", lagrad_avg / enzyme_avg);
+  printf("Slowdown: %f\n", lagrad_avg / enzyme_avg);
 
-  // printf("adjoint for icf: (%lld x %lld x %lld):\n", primal_result.size_0,
-  //        primal_result.size_1, primal_result.size_2);
-  // double *collapsed = collapse_ltri(primal_result);
-  // It's easier just to look at a single collapsed matrix in the batch.
-  // print_d_arr(collapsed, icf_size);
-
-  // free(alphasb);
-  // free(meansb);
-  // free(icfb);
+  free(alphasb);
+  free(meansb);
+  free(icfb);
   free_gmm_input(gmm_input);
   free(ref_alphas);
   free(ref_means);
   free(ref_icf);
 }
-
-// extern F32Descriptor0D gmm_objective(
-//     /*alphas=*/double *, double *, int64_t, int64_t, int64_t, int64_t,
-//     /*means=*/double *, double *, int64_t, int64_t, int64_t, int64_t,
-//     int64_t,
-//     /*Qs=*/double *, double *, int64_t, int64_t, int64_t, int64_t, int64_t,
-//     /*Ls*/ double *, double *, int64_t, int64_t, int64_t, int64_t, int64_t,
-//     int64_t, int64_t,
-//     /*x=*/double *, double *, int64_t, int64_t, int64_t, int64_t, int64_t,
-//     /*wishart_gamma=*/double,
-//     /*wishart_m=*/int64_t);
